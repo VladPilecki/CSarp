0d7d78d8d5a29d960233704b28acfd931d1c2c89
VladPilecki <63882978+VladPilecki@users.noreply.github.com>
2020-05-06 17:39:41 +0300

    Add files via upload

---
+++ 2 laba/laba 2.2/laba 2.2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+
+            string lettersRUS = "ЁЙЦУКЕНГШЩЗХЪЭЖДЛОРПАВЫФЯЧСМИТЬБЮ";
+
+            str = Console.ReadLine();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                for (int j = 0; j < lettersRUS.Length; j++)
+                {
+                    if (str[i] == lettersRUS[j])
+
+                        Console.Write(str[i]);
+                   
+                }
+                
+            }
+            Console.ReadKey();
+        }
+        
+    }
+    
+}
---
+++ 2 laba/laba 2.3/laba 2.3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            string letters = "abcdefghijklmnopqrstuvwxyz";
+            string res;
+            int letter;
+
+            for (int i = 1; i <= 4; i++)
+            {
+                letter = random.Next(0, 26);
+                res = String.Concat(letters[letter]);
+                Console.Write(res);
+            }
+            Console.ReadKey();
+        }
+       
+    }
+   
+}
